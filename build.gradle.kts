cbc02f4868603f876865326a16c3727e76e9946a
Andrey Beryukhov <beryukhov-andrey@yandex.ru>
2021-12-05 14:33:25 +0300

    Migrate to 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
-    val jetbrains_compose_version by extra("1.0.0-beta1")
+    val jetbrains_compose_version by extra("1.0.0")
     val kotlin_version by extra("1.5.31")
     val coroutines_version by extra("1.5.2")
     val appcompat_version by extra("1.3.0")
